f9af4fcc9bbbe2fbe89b399f3e7ba83634bf73bf
Jermaine <jermzblake@gmail.com>
2020-12-05 15:58:27 -0500

    add entry form NavLink to NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,6 +9,7 @@ const NavBar = (props) => {
         <Link to='/login' className='NavBar-link' onClick={props.handleLogout}>LOG OUT</Link>
         &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
         <span className='NavBar-welcome'>WELCOME, {props.user.name}</span>
+        <NavLink to='/entry-form' className='NavBar-link' activeClassName='selected'>ADD ENTRY</NavLink>
       </div>
       :
       <div>
